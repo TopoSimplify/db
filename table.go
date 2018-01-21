f33c9fd46832d9f589dc07f82d83ac3db35863c2
Titus Tienaah <titus.intdxdt@gmail.com>
2018-01-21 17:26:48 -0400

    make u_constraint_table

--- table.go
+++ table.go
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS {{.Table}} (
     status      INT DEFAULT 0,
     snapshot    INT DEFAULT 0,
     CONSTRAINT  pid_{{.Table}}  PRIMARY KEY (id),
-	CONSTRAINT  u_constraint    UNIQUE  (fid, i, j)
+	CONSTRAINT  u_constraint_{{.Table}}    UNIQUE  (fid, i, j)
 ) WITH (OIDS=FALSE);
 CREATE INDEX idx_i_{{.Table}} ON {{.Table}} (i);
 CREATE INDEX idx_j_{{.Table}} ON {{.Table}} (j);
